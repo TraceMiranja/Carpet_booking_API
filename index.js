38f95bab902f2c25d363ad19ab4b0c696c8ee409
Lucas David Erkana <lderkana21x@gmail.com>
2023-12-30 10:16:28 +0200

    Now combine all the routes in one place, index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+// Import required modules
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const customerRoutes = require('./routes/customerRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// Connect to MongoDB
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+// Use routes for handling paths
+app.use('/', customerRoutes);
+app.use('/', bookingRoutes);
+
+// General error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Connect to the database and start the server
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`);
+    });
+});
