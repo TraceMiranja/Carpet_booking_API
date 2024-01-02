82523a1d42e8ae3b7380fc2a17cf5af57df991bc
Lucas David Erkana <lderkana21x@gmail.com>
2024-01-02 12:14:22 +0200

    Add 3 fields
    -Add status
    -Add booking_date
    -Add booking_time

--- models/bookings.js
+++ models/bookings.js
@@ -1,12 +1,44 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-  customer_ID: { type: mongoose.Schema.Types.ObjectId, ref: 'Customer', required: true },
-  apartment: { type: String, required: true },
-  houseNumber: { type: String, required: true },
-  carpetSize: { type: String, required: true },
-  collectionTime: { type: Date, required: true },
-  location: { type: String, required: true }
+  customer_ID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer',
+    required: true
+  },
+  apartment: {
+    type: String,
+    required: true
+  },
+  houseNumber: {
+    type: String,
+    required: true
+  },
+  carpetSize: {
+    type: String,
+    required: true
+  },
+  collectionTime: {
+    type: Date,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  booking_date: {
+    type: Date,
+    default: Date.now // Automatically sets to current date
+  },
+  booking_time: {
+    type: String, // Assuming time is stored in HH:MM format
+    default: () => new Date().toLocaleTimeString() // Automatically sets to current time
+  },
+  status: {
+    type: String,
+    enum: ['incomplete', 'complete', 'pending'],
+    default: 'pending'
+  }
 });
 
 module.exports = mongoose.model('Booking', bookingSchema);
