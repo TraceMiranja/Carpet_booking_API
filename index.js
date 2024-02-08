9e455479cfb4622f3ef83e104b0a03e72aa3ca63
Tracy-miranja <barazatracy44@gmail.com>
2024-02-08 14:21:00 +0300

    Add orign for front end

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // Import required modules
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const customerRoutes = require('./routes/customerRoutes');
-const bookingRoutes = require('./routes/bookingRoutes');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const customerRoutes = require("./routes/customerRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,35 +15,37 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configure CORS to allow specific origins
-app.use(cors({
-  origin: 'https://65afad72d916fd031249fd3e--reliable-fudge-76ec88.netlify.app' // Add your frontend URL here
-}));
+app.use(
+  cors({
+    origin: "https://reliable-fudge-76ec88.netlify.app/", // Add your frontend URL here
+  })
+);
 
 // Connect to MongoDB
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
 };
 
 // Use routes for handling paths
-app.use('/', customerRoutes);
-app.use('/', bookingRoutes);
+app.use("/", customerRoutes);
+app.use("/", bookingRoutes);
 
 // General error handler
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Connect to the database and start the server
 connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log(`Listening on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
 });
