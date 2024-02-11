6c27251c596b67a4118079ab6647fa36e34d0841
Tracy-miranja <barazatracy44@gmail.com>
2024-02-11 22:11:08 +0300

    Add price

--- models/bookings.js
+++ models/bookings.js
@@ -1,44 +1,48 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
   customer_ID: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Customer',
-    required: true
+    ref: "Customer",
+    required: true,
   },
   apartment: {
     type: String,
-    required: true
+    required: true,
   },
   houseNumber: {
     type: String,
-    required: true
+    required: true,
   },
   carpetSize: {
     type: String,
-    required: true
+    required: true,
+  },
+  price: {
+    type: Number, // Define price as a Number type
+    required: true,
   },
   collectionTime: {
     type: Date,
-    required: true
+    required: true,
   },
   location: {
     type: String,
-    required: true
+    required: true,
   },
   booking_date: {
     type: Date,
-    default: Date.now // Automatically sets to current date
+    default: Date.now, // Automatically sets to current date
   },
   booking_time: {
     type: String, // Assuming time is stored in HH:MM format
-    default: () => new Date().toLocaleTimeString() // Automatically sets to current time
+    default: () => new Date().toLocaleTimeString(), // Automatically sets to current time
   },
   status: {
     type: String,
-    enum: ['incomplete', 'complete', 'pending'],
-    default: 'pending'
-  }
+    enum: ["incomplete", "complete", "pending"],
+    default: "pending",
+  },
 });
 
-module.exports = mongoose.model('Booking', bookingSchema);
+module.exports = mongoose.model("Booking", bookingSchema);
